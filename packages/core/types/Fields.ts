1c14e4e216d607fdfe67231b7bf60bd1b43540d8
Chris Villa <chrisvilla@me.com>
2025-07-09 19:51:10 +0100

    fix: support user-defined fields in overrides
    Includes the new ConfigWithExtensions and ComponentConfigWithExtensions types to enable the user to provide custom field types.

--- packages/core/types/Fields.ts
+++ packages/core/types/Fields.ts
@@ -141,11 +141,14 @@ export type Field<Props extends any = any> =
   | SlotField;
 
 export type Fields<
-  ComponentProps extends DefaultComponentProps = DefaultComponentProps
+  ComponentProps extends DefaultComponentProps = DefaultComponentProps,
+  UserField extends {} = {}
 > = {
-  [PropName in keyof Omit<ComponentProps, "editMode">]: Field<
-    ComponentProps[PropName]
-  >;
+  [PropName in keyof Omit<ComponentProps, "editMode">]: UserField extends {
+    type: string;
+  }
+    ? Field<ComponentProps[PropName]> | UserField
+    : Field<ComponentProps[PropName]>;
 };
 
 export type FieldProps<F = Field<any>, ValueType = any> = {
