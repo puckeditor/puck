ddee601ed534b263e0abedf9ec2d494a334b1cba
Chris Villa <chrisvilla@me.com>
2025-07-29 19:13:37 +0100

    feat: add new side menu

--- packages/core/components/Puck/components/Layout/index.tsx
+++ packages/core/components/Puck/components/Layout/index.tsx
@@ -19,6 +19,8 @@ import { Fields } from "../Fields";
 import { useSidebarResize } from "../../../../lib/use-sidebar-resize";
 import { FrameProvider } from "../../../../lib/frame-context";
 import { Sidebar } from "../Sidebar";
+import { Nav } from "../Nav";
+import { Hammer, Heading1, Layers } from "lucide-react";
 
 const getClassName = getClassNameFactory("Puck", styles);
 const getLayoutClassName = getClassNameFactory("PuckLayout", styles);
@@ -135,6 +137,8 @@ export const Layout = ({ children }: { children: ReactNode }) => {
 
   usePreviewModeHotkeys();
 
+  const [view, setView] = useState<"blocks" | "outline" | "headings">("blocks");
+
   return (
     <div className={`Puck ${getClassName()}`}>
       <DragDropContext disableAutoScroll={dnd?.disableAutoScroll}>
@@ -150,27 +154,53 @@ export const Layout = ({ children }: { children: ReactNode }) => {
               >
                 <div
                   className={getLayoutClassName("inner")}
-                  style={{
-                    gridTemplateColumns: `
-                    ${
-                      leftSideBarVisible
-                        ? leftWidth
-                          ? `${leftWidth}px`
-                          : "var(--puck-side-bar-width)"
-                        : "0"
-                    } 
-                    var(--puck-frame-width) 
-                    ${
-                      rightSideBarVisible
-                        ? rightWidth
-                          ? `${rightWidth}px`
-                          : "var(--puck-side-bar-width)"
-                        : "0"
-                    }
-                  `,
-                  }}
+                  style={
+                    {
+                      "--puck-user-left-side-bar-width": leftWidth
+                        ? `${leftWidth}px`
+                        : "auto",
+                      "--puck-user-right-side-bar-width": rightWidth
+                        ? `${rightWidth}px`
+                        : "auto",
+                    } as any
+                  }
                 >
                   <Header />
+                  <div className={getLayoutClassName("nav")}>
+                    <Nav
+                      slim
+                      navigation={{
+                        main: {
+                          items: {
+                            build: {
+                              label: "Blocks",
+                              icon: <Hammer />,
+                              onClick: () => {
+                                setView("blocks");
+                              },
+                              isActive: view === "blocks",
+                            },
+                            outline: {
+                              label: "Outline",
+                              icon: <Layers />,
+                              onClick: () => {
+                                setView("outline");
+                              },
+                              isActive: view === "outline",
+                            },
+                            headings: {
+                              label: "Headings",
+                              icon: <Heading1 />,
+                              onClick: () => {
+                                setView("headings");
+                              },
+                              isActive: view === "headings",
+                            },
+                          },
+                        },
+                      }}
+                    />
+                  </div>
                   <Sidebar
                     position="left"
                     sidebarRef={leftSidebarRef}
@@ -179,12 +209,12 @@ export const Layout = ({ children }: { children: ReactNode }) => {
                     onResize={setLeftWidth}
                     onResizeEnd={handleLeftSidebarResizeEnd}
                   >
-                    <SidebarSection title="Components" noBorderTop>
-                      <Components />
-                    </SidebarSection>
-                    <SidebarSection title="Outline">
-                      <Outline />
-                    </SidebarSection>
+                    {leftSideBarVisible && (
+                      <>
+                        {view === "blocks" && <Components />}
+                        {view === "outline" && <Outline />}
+                      </>
+                    )}
                   </Sidebar>
                   <Canvas />
                   <Sidebar
