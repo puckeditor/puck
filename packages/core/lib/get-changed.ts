35bc00be870a43761524f8567edf3a32f65b87a1
Marko Trebizan <mtrebizan@gmail.com>
2025-08-29 13:27:29 +0200

    revert formatting changes

--- packages/core/lib/get-changed.ts
+++ packages/core/lib/get-changed.ts
@@ -2,18 +2,18 @@ import { ComponentData } from "../types";
 import { deepEqual } from "fast-equals";
 
 export const getChanged = (
-	newItem: Omit<Partial<ComponentData<any>>, "type"> | undefined,
-	oldItem: Omit<Partial<ComponentData<any>>, "type"> | null | undefined,
+  newItem: Omit<Partial<ComponentData<any>>, "type"> | undefined,
+  oldItem: Omit<Partial<ComponentData<any>>, "type"> | null | undefined
 ) => {
-	return newItem
-		? Object.keys(newItem.props || {}).reduce((acc, item) => {
-				const newItemProps = newItem?.props || {};
-				const oldItemProps = oldItem?.props || {};
+  return newItem
+    ? Object.keys(newItem.props || {}).reduce((acc, item) => {
+        const newItemProps = newItem?.props || {};
+        const oldItemProps = oldItem?.props || {};
 
-				return {
-					...acc,
-					[item]: !deepEqual(oldItemProps[item], newItemProps[item]),
-				};
-			}, {})
-		: {};
+        return {
+          ...acc,
+          [item]: !deepEqual(oldItemProps[item], newItemProps[item]),
+        };
+      }, {})
+    : {};
 };
