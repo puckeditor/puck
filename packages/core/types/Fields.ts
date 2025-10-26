32895d909729c452c3b9b24dae8c646e267b975e
Hugo <hugodebard.pro@gmail.com>
2025-10-26 17:31:27 +0100

    fix: update Select component styles and enhance FieldOption type to support ReactNode

--- packages/core/types/Fields.ts
+++ packages/core/types/Fields.ts
@@ -1,8 +1,8 @@
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
 import { DefaultComponentProps, Metadata, UiState } from ".";
 
 type FieldOption = {
-  label: string;
+  label: string | ReactNode;
   value: string | number | boolean | undefined | null | object;
 };
 
