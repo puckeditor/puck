d701a1daea4aad3375db96e67c77bc1f26a0b8a8
Federico Jorge Bonel Tozzi <bonelfederico@gmail.com>
2026-01-12 15:05:38 +0900

    feat: add puck ai option to create-puck-app

---
+++ recipes/remix-ai/app/routes/api.puck.$.tsx
@@ -0,0 +1,11 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { puckHandler } from "@puckeditor/cloud-client";
+
+export async function action(args: ActionFunctionArgs) {
+  return puckHandler(args.request, {
+    ai: {
+      // Replace with your business context
+      context: "We are Google. You create Google landing pages.",
+    },
+  });
+}
