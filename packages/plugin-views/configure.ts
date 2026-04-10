cf748578517e06db76d0a87b46d4fcc0b5c1146b
Federico Jorge Bonel Tozzi <bonelfederico@gmail.com>
2026-04-10 15:11:28 +0900

    performance: make withViews more performant

--- packages/plugin-views/configure.ts
+++ packages/plugin-views/configure.ts
@@ -1,4 +1,9 @@
-import type { Config, RootData } from "@puckeditor/core";
+import type {
+  ComponentConfig,
+  Config,
+  RootConfig,
+  RootData,
+} from "@puckeditor/core";
 import { toRootComponent } from "./src/lib/views";
 import {
   applyNodeViews,
@@ -7,53 +12,52 @@ import {
 } from "./src/lib/views";
 import type { ViewsPluginOptions } from "./src/types";
 
-const wrapResolveData = (
-  configItem: Record<string, any> | undefined,
+const wrapResolveData = <T extends ComponentConfig | RootConfig>(
+  configItem: T,
   options: ViewsPluginOptions,
   isRoot: boolean = false
-) => {
-  if (!configItem) {
-    return configItem;
-  }
-
+): T => {
   const existingResolveData = configItem.resolveData;
 
-  return {
-    ...configItem,
-    resolveData: async (data: any, params: any) => {
-      const baseData = existingResolveData
-        ? await existingResolveData(data, params)
-        : data;
-      const mergedData = {
-        ...data,
-        ...baseData,
-        props: {
-          ...data.props,
-          ...(baseData?.props ?? {}),
-        },
-      };
-      const root =
-        isRoot && mergedData.props
-          ? toRootComponent({
-              props: {
-                ...mergedData.props,
-              },
-              readOnly: mergedData.readOnly,
-            } as RootData)
-          : params.root;
+  const newConfigItem = { ...configItem } as ComponentConfig;
 
-      return applyNodeViews({
-        data: mergedData,
-        metadata: params.metadata,
-        root,
-        options: {
-          ...options,
-          nodeStateKey: options.nodeStateKey ?? DEFAULT_NODE_STATE_KEY,
-          storageKey: options.storageKey ?? DEFAULT_STORAGE_KEY,
-        },
-      });
-    },
+  newConfigItem.resolveData = async (data, params) => {
+    const baseData = existingResolveData
+      ? await existingResolveData(data, params)
+      : data;
+
+    const mergedData = {
+      ...data,
+      ...baseData,
+      props: {
+        ...data.props,
+        ...(baseData?.props ?? {}),
+      },
+    };
+
+    const root =
+      isRoot && mergedData.props
+        ? toRootComponent({
+            props: {
+              ...mergedData.props,
+            },
+            readOnly: mergedData.readOnly,
+          } as RootData)
+        : params.root;
+
+    return applyNodeViews({
+      data: mergedData,
+      metadata: params.metadata,
+      root,
+      options: {
+        ...options,
+        nodeStateKey: options.nodeStateKey ?? DEFAULT_NODE_STATE_KEY,
+        storageKey: options.storageKey ?? DEFAULT_STORAGE_KEY,
+      },
+    });
   };
+
+  return newConfigItem as T;
 };
 
 export const withViews = <UserConfig extends Config>(
@@ -66,17 +70,20 @@ export const withViews = <UserConfig extends Config>(
     storageKey: options.storageKey ?? DEFAULT_STORAGE_KEY,
   };
 
-  return {
-    ...config,
-    root: wrapResolveData(config.root, normalizedOptions, true),
-    components: Object.entries(config.components).reduce<
-      UserConfig["components"]
-    >(
-      (acc, [componentName, componentConfig]) => ({
-        ...acc,
-        [componentName]: wrapResolveData(componentConfig, normalizedOptions),
-      }),
-      {} as UserConfig["components"]
-    ),
-  };
+  const newConfig: UserConfig = { ...config };
+
+  if (config.root) {
+    newConfig.root = wrapResolveData(config.root, normalizedOptions, true);
+  }
+
+  Object.entries(config.components).forEach(
+    ([componentName, componentConfig]) => {
+      newConfig.components[componentName] = wrapResolveData(
+        componentConfig,
+        normalizedOptions
+      );
+    }
+  );
+
+  return newConfig;
 };
