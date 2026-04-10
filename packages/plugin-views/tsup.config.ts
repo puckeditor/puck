444eaf5d2aac91c595fad42888dcd587896e8374
Federico Jorge Bonel Tozzi <bonelfederico@gmail.com>
2026-04-10 18:18:45 +0900

    chore: fix tsconfig.json and tsup.config.ts to be consistent with core package

--- packages/plugin-views/tsup.config.ts
+++ packages/plugin-views/tsup.config.ts
@@ -1,7 +1,4 @@
 import { defineConfig } from "tsup";
-import tsupconfig from "tsup-config";
+import tsupconfig from "../tsup-config";
 
-export default defineConfig({
-  ...tsupconfig,
-  dts: { resolve: true },
-} as any);
+export default defineConfig({ ...tsupconfig });
