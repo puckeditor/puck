29a01e80fa6005c594a6713a1b00a951ed082451
Federico Jorge Bonel Tozzi <bonelfederico@gmail.com>
2026-06-03 09:39:32 +0900

    chore: upgrade recipes to puck-ai 0.7

--- recipes/next-ai/app/api/puck/[...all]/route.ts
+++ recipes/next-ai/app/api/puck/[...all]/route.ts
@@ -1,13 +1,23 @@
-import { NextRequest } from "next/server";
-import { puckHandler } from "@puckeditor/cloud-client";
-
 // Handles all requests for Puck AI
 // Learn more: https://puckeditor.com/docs/ai/getting-started
+import { NextRequest } from "next/server";
+import { puckHandler, PuckCloudOptions } from "@puckeditor/cloud-client";
+
+const options: PuckCloudOptions = {
+  ai: {
+    // Replace with your business context
+    context: "We are Google. You create Google landing pages.",
+  },
+};
+
+export const GET = (request: NextRequest) => {
+  return puckHandler(request, options);
+};
+
 export const POST = (request: NextRequest) => {
-  return puckHandler(request, {
-    ai: {
-      // Replace with your business context
-      context: "We are Google. You create Google landing pages.",
-    },
-  });
+  return puckHandler(request, options);
+};
+
+export const DELETE = (request: NextRequest) => {
+  return puckHandler(request, options);
 };
