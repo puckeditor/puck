4d30fc17d9bd30ee749b05c2a694da4d9657d2a1
Federico Jorge Bonel Tozzi <bonelfederico@gmail.com>
2026-06-05 14:10:07 +0900

    chore: refactor puckHandler to follow same pattern as the docs

--- recipes/next-ai/app/api/puck/[...all]/route.ts
+++ recipes/next-ai/app/api/puck/[...all]/route.ts
@@ -1,23 +1,16 @@
 // Handles all requests for Puck AI
 // Learn more: https://puckeditor.com/docs/ai/getting-started
-import { NextRequest } from "next/server";
-import { puckHandler, PuckCloudOptions } from "@puckeditor/cloud-client";
+import { puckHandler } from "@puckeditor/cloud-client";
 
-const options: PuckCloudOptions = {
-  ai: {
-    // Replace with your business context
-    context: "We are Google. You create Google landing pages.",
-  },
+const handleRequest = (request) => {
+  return puckHandler(request, {
+    ai: {
+      // Replace with your business context
+      context: "We are Google. You create Google landing pages.",
+    },
+  });
 };
 
-export const GET = (request: NextRequest) => {
-  return puckHandler(request, options);
-};
-
-export const POST = (request: NextRequest) => {
-  return puckHandler(request, options);
-};
-
-export const DELETE = (request: NextRequest) => {
-  return puckHandler(request, options);
-};
+export const DELETE = handleRequest;
+export const GET = handleRequest;
+export const POST = handleRequest;
