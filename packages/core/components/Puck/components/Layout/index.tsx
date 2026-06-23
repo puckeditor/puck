3faf7de0204e0e3d823f6ef556c415139c47c37c
Federico Jorge Bonel Tozzi <bonelfederico@gmail.com>
2026-06-23 17:56:05 +0900

    refactor: rename sidebar css properties to follow sidebar-side format

--- packages/core/components/Puck/components/Layout/index.tsx
+++ packages/core/components/Puck/components/Layout/index.tsx
@@ -172,11 +172,11 @@ export const Layout = ({ children }: { children?: ReactNode }) => {
   const layoutOptions: Record<string, any> = {};
 
   if (leftWidth) {
-    layoutOptions["--puck-user-left-sidebar-width"] = `${leftWidth}px`;
+    layoutOptions["--puck-user-sidebar-left-width"] = `${leftWidth}px`;
   }
 
   if (rightWidth) {
-    layoutOptions["--puck-user-right-sidebar-width"] = `${rightWidth}px`;
+    layoutOptions["--puck-user-sidebar-right-width"] = `${rightWidth}px`;
   }
 
   const setUi = useAppStore((s) => s.setUi);
